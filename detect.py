e34d6e795c0ac4485fee133ce167782ebc2634f4
zhangjie <545277714@qq.com>
2020-07-09 17:54:39 +0800

    finish add tracking for detection

--- detect.py
+++ detect.py
@@ -1,3 +1,5 @@
+from sort import *
+
 import argparse
 
 import torch.backends.cudnn as cudnn
@@ -24,6 +26,7 @@ def detect(save_img=False):
     os.makedirs(out)  # make new output folder
     half = device.type != 'cpu'  # half precision only supported on CUDA
     print(os.path.isfile(weights))
+    mot_tracker = Sort()
     # Load model
  #   google_utils.attempt_download(weights)
     model = torch.load(weights, map_location=device)['model'].float()  # load to FP32
@@ -95,27 +98,33 @@ def detect(save_img=False):
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
                     s += '%g %ss, ' % (n, names[int(c)])  # add to string
+                
 
+                track_det = [elem[:5].tolist() for elem in det if elem[5] == 0]
+                track_det = np.array(track_det)
+                track_bbs_ids = mot_tracker.update(track_det)
+      
                 person_count = 0
                 # Write results
-                for *xyxy, conf, cls in det:
+                for *xyxy, pid in track_bbs_ids:
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * 5 + '\n') % (cls, *xywh))  # label format
 
                     if save_img or view_img:  # Add bbox to image
-                        if int(cls)==0: #change by miao   plot only person in image
+                        #if int(cls)==0: #change by miao   plot only person in image
                             #label = '%s %.2f' % (names[int(cls)], conf)
-                            label = '%.2f' % (conf)
-                            plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
-                            x1, y1, x2, y2 = int(xyxy[0]), int(xyxy[1]), int(xyxy[2]), int(xyxy[3])
-                            # print(im0.shape)
-                            bbox_img = im0[y1:y2,x1:x2]
+                        label = '%d' % (pid)
+                        plot_one_box(xyxy, im0, label=label, color=colors[0], line_thickness=1)
+                        height, width, _ = im0.shape
+                        x1, y1, x2, y2 = max(0,int(xyxy[0])), max(0,int(xyxy[1])), min(width,int(xyxy[2])), min(height,int(xyxy[3]))
+    
+                        bbox_img = im0[y1:y2,x1:x2]
                             # print(type(bbox_img))
-                            shortname, extension = os.path.splitext(Path(p).name)
-                            person_count += 1
-                            bbox_img_name = shortname + '_person%d'%(person_count) + extension
+                        shortname, extension = os.path.splitext(Path(p).name)
+                        person_count += 1
+                        bbox_img_name = shortname + '_person%d'%(person_count) + extension
                             # print(bbox_img_name)
                             # bbox_img_path = str(Path(out) / Path(bbox_img_name))
                             # cv2.imwrite(bbox_img_path, bbox_img)
@@ -129,22 +138,22 @@ def detect(save_img=False):
                             # score = model_deepmar(img_var).data.cpu().numpy()
                             # print(score)
 
-                            bbox_img_convt_PIL = Image.fromarray(cv2.cvtColor(bbox_img,cv2.COLOR_BGR2RGB))
-                            img_trans_conv_PIL = input_transform(bbox_img_convt_PIL)
-                            img_trans_conv_PIL = torch.unsqueeze(img_trans_conv_PIL, dim=0)
-                            img_trans_conv_PIL_var = Variable(img_trans_conv_PIL).cuda()
-                            score = model_deepmar(img_trans_conv_PIL_var).data.cpu().numpy()
+                        bbox_img_convt_PIL = Image.fromarray(cv2.cvtColor(bbox_img,cv2.COLOR_BGR2RGB))
+                        img_trans_conv_PIL = input_transform(bbox_img_convt_PIL)
+                        img_trans_conv_PIL = torch.unsqueeze(img_trans_conv_PIL, dim=0)
+                        img_trans_conv_PIL_var = Variable(img_trans_conv_PIL).cuda()
+                        score = model_deepmar(img_trans_conv_PIL_var).data.cpu().numpy()
                             # print(score)
                             # result_att = []
                             # print(bbox_img_path+" done attribution recognition")
-                            att_count = -1
-                            for idx in range(len(att_list)):
-                                if score[0, idx] >= 0:
-                                    txt = '%s: %.2f' % (att_list[idx], score[0, idx])
+                        att_count = -1
+                        for idx in range(len(att_list)):
+                            if score[0, idx] >= 0:
+                                txt = '%s: %.2f' % (att_list[idx], score[0, idx])
                                     # print(txt)
-                                    att_count += 1
+                                att_count += 1
                                     # print(att_count)
-                                    cv2.putText(im0, att_list[idx], (x1, y1 + 5 + 10*att_count ), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, [225, 255, 255],
+                                cv2.putText(im0, att_list[idx], (x1, y1 + 5 + 10*att_count ), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, [225, 255, 255],
                                                 thickness=1, lineType=cv2.LINE_AA)
                                     # cv2.imshow('deepmar',im0)
                                     # cv2.waitKey()
@@ -231,3 +240,4 @@ if __name__ == '__main__':
         # for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt', 'yolov3-spp.pt']:
         #    detect()
         #    create_pretrained(opt.weights, opt.weights)
+
